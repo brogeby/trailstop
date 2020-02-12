0af691a4143e2ce9840fa05dd47a2108083a3581
Brogeby <olof.brogeby@cmeducations.se>
2020-02-12 14:20:41 +0100

    Added picture to the product page

--- product-page.php
+++ product-page.php
@@ -6,7 +6,7 @@
     <title>Trailstop | Rocky Mountain Altitude</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
-    <link rel='stylesheet' type='text/css' media='screen' href='css/product-page.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/product.css'>
 
 </head>
 <body>
@@ -15,4 +15,9 @@
     include "parts/header.php";
     include "parts/phone_menu.php";
 ?>
+
+<section class="product-image" id="altitude-carbon-90">
+    <img src="img\Web_MY20_Altitude_C90_C2_Profile_0.jpg" alt="">
+</section>
+
 </html>
