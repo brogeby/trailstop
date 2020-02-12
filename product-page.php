cd0cc18dd40ad356ceb93fb7d208a85e8e79a922
Brogeby <olof.brogeby@cmeducations.se>
2020-02-12 14:02:18 +0100

    Made some minor aligning issues on header

--- product-page.php
+++ product-page.php
@@ -5,8 +5,14 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Trailstop | Rocky Mountain Altitude</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/product-page.css'>
+
 </head>
 <body>
-    <?php include "parts/header.php";?>
+<script src="js/phone_menu.js"></script>
+<?php 
+    include "parts/header.php";
+    include "parts/phone_menu.php";
+?>
 </html>
