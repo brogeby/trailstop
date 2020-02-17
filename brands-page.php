8c46a7d9d56d17761d4e4cf1e4686b7ff0a404fe
Amipalsson <60134304+Amipalsson@users.noreply.github.com>
2020-02-17 09:07:28 +0100

    Brands page

--- brands-page.php
+++ brands-page.php
@@ -5,9 +5,51 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Trailstop</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/brands.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
 </head>
 <body>
+    <script src="js/phone_menu.js"></script>
+<?php 
+    include "parts/header.php";
+    include "parts/phone_menu.php";
+?> 
+  <div class="wrapper">
+    <div class="cell-2">
+      <div class="logo"> 
+         <a href="https://www.specialized.com/se/en">
+        <img class="specialized" src="img/specialized-logo-1.png" width="200" height="250" alt="Specialized"></a>
+      </div>
+      <div class="logo">
+         <a href="https://www.trekbikes.com/se/sv_SE/">
+         <img class="trek" src="img/trek_logo.png" width="150" height="100" alt="trek"></a>
+      </div>
+        <div class="logo">
+         <a href="https://www.cube.eu/cube-bikes/">
+        <img class="cube" src="img/cube_logo.png" width="200" height="150" alt="Cube"></a>
+      </div>
+        <div class="logo">
+         <a href="https://www.bikes.com/">
+          <img class="rocky" src="img/rocky_logo.png" width="200" height="150" alt="Rocky Mountain"></a>
+        </div>
+        <div class="logo">
+         <a href="https://www.scott-sports.com/se/en/products/bike-bikes">
+          <img class="scott" src="img/scott_logo.png" width="200" height="150" alt="Scott"></a>
+        </div>
+        <div class="logo">
+         <a href="https://www.yeticycles.com/">
+          <img class="trek" src="img/yeti_logo.png" width="200" height="150" alt="Yeti"></a>
+        </div>
+   </div>
+    <footer class="cell-8">
+        <div class="cell-9">Footer1</div>
+        <div class="cell-10">Footer2</div>
+        <div class="cell-11">Footer3</div>
+        <div class="cell-12">Footer4</div>
+    </footer>
+</div>
+  
+
     
 </body>
 </html>
