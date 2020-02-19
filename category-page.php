db6b8f703214958babbc1a7671e559fe106a2815
Annis Said <annissaid@Anniss-MacBook-Pro.local>
2020-02-20 00:15:13 +0100

    klar med layout, ska styla check-box

--- category-page.php
+++ category-page.php
@@ -5,7 +5,8 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Trailstop</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='category.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <link rel="stylesheet" type='text/css' href="css/category.css">
 </head>
 <body>
 <script src="js/phone_menu.js"></script>
@@ -14,30 +15,142 @@
     include "parts/phone_menu.php";
 ?> 
 
+
 <div class="grid-container">
+
     
     
-    <select class="Sortera">
-        <option>Sortera</option>
-        <option>Högst Pris</option>
-        <option>Lägst Pris</option>
+    
+    <select class="filter">
+        <option>Filtrera</option>
+        <option>Herr</option>
+        <option>Dam</option>
+        <option>Junior</option>
+        <option>Storlek</option>
     </select>
     
     
-    <select class="filter">Filtrera
+    <select class="sort">
         <option>Sortera</option>
         <option>Högst Pris</option>
         <option>Lägst Pris</option>		
     </select>
+
+    <section class="category">
+        <h2 class="category-header">Kategori</h2>
+        <div class="filter-max-with">
+            <ul>
+                <div class="sub-header">
+                    <h4>Välj</h4>
+                    <input type="checkbox" checked="checked" class="check-box"><li>Dam</li><br>
+                    <input type="checkbox" checked="checked" class="check-box"><li>Herr</li><br>
+                    <input type="checkbox" checked="checked" class="check-box"><li>Junior</li><br>
+                </div>
+            </ul>
+
+            
+
+            <ul>
+                <div class="sub-header">
+                    <h4>Storlek</h4>
+                    <input type="checkbox" checked="checked" class="check-box"><li>24"</li><br>
+                    <input type="checkbox" checked="checked" class="check-box"><li>26"</li><br>
+                    <input type="checkbox" checked="checked" class="check-box"><li>27,5"</li><br>
+                    <input type="checkbox" checked="checked" class="check-box"><li>28"</li><br>
+                </div>
+            </ul>
+
+            
+            <ul>
+                <h4>Varumärke</h4>
+                <input type="checkbox" checked="checked" class="check-box"><li>Cube</li><br>
+                <input type="checkbox" checked="checked" class="check-box"><li>Rocky Mountain</li><br>
+                <input type="checkbox" checked="checked" class="check-box"><li>Scott</li><br>
+                <input type="checkbox" checked="checked" class="check-box"><li>Specialized</li><br>
+                <input type="checkbox" checked="checked" class="check-box"><li>Trek</li><br>
+                <input type="checkbox" checked="checked" class="check-box"><li>Yeti</li>
+            </ul>
+        </div>
+  
+    </section>
     
     
-    <!--<div class="item4">Produkt 1</div>
-    <div class="item5">Produkt 2</div>
-    <div class="item6">Produkt 3</div>
-    <div class="item7">Produkt 4</div>
-    <div class="item8">Produkt 5</div>
-    <div class="item9">Produkt 6</div>
-    <div class="item10">Footer</div>-->
+    <div class="item4">
+        <a href="http://ludvigolausson.se/product-page.php">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+        </a>
+
+    </div>
+
+    <div class="item5">
+        <a href="http://ludvigolausson.se/product-page.php">
+        <img href="#" src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+        </a>
+    </div>
+
+    <div class="item6">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+
+    <div class="item7">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+
+    <div class="item8">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+
+    <div class="item9">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+
+    <div class="item10">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+
+    <div class="item11">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+    <div class="item12">
+        <img src="https://p.vitalmtb.com/photos/products/27266/photos/50347/s1600_2020_Rocky_Mountain_Altitude_Carbon_90_Black_Brown_and_Orange.jpg?1568159679" class="product">
+        <ul>
+            <li><strong>Altitude Carbon 90</strong></li><br>
+            <li>79 900kr</li>
+        </ul>
+    </div>
+    
 </div>
 
     
