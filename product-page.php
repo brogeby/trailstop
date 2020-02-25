eeab36fe380c05665945aba023cadbc1b20912c3
Brogeby <olof.brogeby@cmeducations.se>
2020-02-25 23:36:31 +0100

    fixed a footer bug on product page

--- product-page.php
+++ product-page.php
@@ -30,41 +30,41 @@
 </section>
 
 <section class="specifications">
-    <h2>Specifications</h2>
+    <h2>Specifikationer</h2>
     <div class="frame">
-        <h3>Frame</h3>
+        <h3>Ram</h3>
         <p>SMOOTHWALL Carbon. Full Sealed Cartridge Bearings. Press Fit BB. Internal Cable Routing. 2-Bolt ISCG05 Tabs. RIDE-9 Adjustable Geometry + Suspension Rate</p>
     </div>
     <div>
-        <h3>Fork</h3>
+        <h3>Gaffel</h3>
         <p>Fox 36 Float EVOL GRIP2 Factory 160mm 37mm Offset</p>
     </div>
     <div>
-        <h3>Shock</h3>
+        <h3>Dämpare</h3>
         <p>Fox Float DPX2 Factory</p>
     </div>
     <div>
-        <h3>Handlebar</h3>
+        <h3>Styre</h3>
         <p>Race Face Next R 780mm</p>
     </div>
     <div>
-        <h3>Shifters</h3>
+        <h3>Växelreglage</h3>
         <p>Shimano XTR</p>
     </div>
     <div>
-        <h3>Rear Derailleur</h3>
+        <h3>Bakväxel</h3>
         <p>Shimano XTR</p>
     </div>
     <div>
-        <h3>Seatpost</h3>
+        <h3>Sadelstolpe</h3>
         <p>Race Face Turbine R (by Fox) Dropper 30.9mm</p>
     </div>
     <div>
-        <h3>Brakes</h3>
+        <h3>Bromsar</h3>
         <p>Shimano XTR Trail 4 Piston / Shimano RT86 180mm / Shimano RT86 180mm</p>
     </div>
     <div>
-        <h3>Tires</h3>
+        <h3>Däck</h3>
         <p>Maxxis Minion DHF WT Maxx Terra 3C Tubeless Ready 27.5 x 2.5 / Maxxis Aggressor WT EXO Tubeless Ready 27.5 x 2.5</p>
     </div>
 </section>
